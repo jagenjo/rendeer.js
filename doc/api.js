c1ff702f3881d6d451b3ae1bcb46560bae50d38d
tamat <contact@tamats.com>
2015-03-30 13:29:07 +0200

    fixed namespaces, now all start with RD

---
+++ doc/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Billboard",
+        "Camera",
+        "ParticlesEmissor",
+        "PointCloud",
+        "RD",
+        "Renderer",
+        "Scene",
+        "SceneNode",
+        "Sprite"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
