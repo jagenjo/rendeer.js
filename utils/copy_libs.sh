e239f33c0c85c6776ac055b95d04e5b7a08b0878
tamat <contact@tamats.com>
2021-02-22 14:30:37 +0100

    added wbin as external

--- utils/copy_libs.sh
+++ utils/copy_libs.sh
@@ -1,6 +1,7 @@
 ../../litegl/utils/build.sh
 cp -v ../../litegl/build/* ../external/
 cp -v ../../litegl/external/gl-matrix* ../external/
+cp -v ../../litegl/external/wbin.js ../external/
 cp -v ../external/* ../editor/js/libs/
 cp -v ../../hdre/src/hdre.js ../external/
 
